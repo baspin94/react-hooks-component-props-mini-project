16ac00812dc194afdaf12fb0d0a3f4a1765f8830
Bianca Aspin <bianca@mj94.net>
2023-02-02 21:07:51 -0800

    Complete lab.

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+const defaultImage = "https://via.placeholder.com/215"
+
+function About({image = defaultImage, about}) {
+    return (
+        <aside>
+            <img src={image} alt="blog logo"/>
+            <p>{about}</p>
+        </aside>
+
+    )
+}
+
+export default About;
